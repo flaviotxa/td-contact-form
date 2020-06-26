93aae620582ebdc5bba54913dc8246dab09a9741
Flavio Teixeira Lopes <flaviotxa@gmail.com>
2020-06-26 13:29:08 +0100

    Added the submit action

--- assets/api.js
+++ assets/api.js
@@ -30,4 +30,45 @@ const userAction = async () => {
   });
 };
 
+const submitForm = async (e) => {
+  if (e.preventDefault) e.preventDefault();
+
+  const field_list = document
+    .getElementById("field_list")
+    .getElementsByTagName("li");
+
+  var fields = [];
+
+  for (var i = 0; i < field_list.length; i++) {
+    var input = field_list[i].getElementsByTagName("input");
+
+    var field = {};
+
+    field["id"] = input[0].id;
+    field["field_value"] = input[0].value;
+
+    fields.push(field);
+  }
+
+  var form_fields = {};
+  form_fields["fields"] = fields;
+
+  console.log(JSON.stringify(form_fields));
+
+  const response = await fetch(`http://localhost:3333/forms/${formId}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(form_fields),
+  });
+
+  document.body.innerHTML = "<p>Thanks for the reply!</p>";
+};
+
+var form = document.getElementById("form");
+if (form.attachEvent) {
+  form.attachEvent("submit", submitForm);
+} else {
+  form.addEventListener("submit", submitForm);
+}
+
 userAction();
