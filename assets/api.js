ddda9174bb7abee6e6874c6dde28c7427919a058
Flavio Teixeira Lopes <flaviotxa@gmail.com>
2020-06-26 09:49:38 +0100

    Updated the Api to get as single form

--- assets/api.js
+++ assets/api.js
@@ -5,9 +5,9 @@ const urlParams = new URLSearchParams(queryString);
 const formId = urlParams.get("id");
 
 const userAction = async () => {
-  const response = await fetch("http://localhost:3333/forms");
-  const myJson = await response.json();
-  let { fields, title } = myJson[0];
+  const response = await fetch(`http://localhost:3333/forms/${formId}`);
+  const form = await response.json();
+  let { fields, title } = form;
 
   document.getElementById("form_title").innerHTML = title;
   var field_list = document.getElementById("field_list");
@@ -18,7 +18,7 @@ const userAction = async () => {
     li.innerHTML += `<label>${field.label} </label>
       <div>
         <input
-          id="element_1"
+          id="${field._id}"
           name="element_1"
           type="${field.field_type}"
           maxlength="255"
